143d925d2eb3b1c9fe07f1a26ee7ca5c8dfdfa70
Gitali03 <aonermtk@gmail.com>
2024-12-17 15:49:46 +0300

    added api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/posts",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
